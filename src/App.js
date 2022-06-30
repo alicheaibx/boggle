2879352cc557d80952fa90fc5863265a3bb862de
unknown <41730625@students.liu.edu.lb>
2022-06-30 02:41:36 -0700

    finsh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import logo from "./img/logo.png";
 import "./App.css";
-import Game from "./components/Game";
 import Timer from "./components/Timer";
 
+
 class App extends Component {
+  stylelogo={width:200,
+  highit:200
+  };
   render() {
     return (
       <div className="container">
-     
+        <div className="header">
+         
+          <img style={this.stylelogo} src={logo} className="header-logo" alt="logo" />
+        </div>
+        
         <Timer />
       </div>
     );
